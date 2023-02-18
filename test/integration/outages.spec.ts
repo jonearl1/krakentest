304ce1d1a21875c36f9379455248789900a1cdd9
Jon Earl <jon.r.earl@gmail.com>
2023-02-18 21:45:49 +0000

    integration test against real endpoint

--- test/integration/outages.spec.ts
+++
@@ -1,8 +0,0 @@
-import getOutages from '../../src/outage-service';
-
-describe('Outage Service', () => {
-  it('should retrieve outages from kraken api /outages endpoint', async () => {
-    const result = await getOutages();
-    expect(result.length).toBeGreaterThan(6);
-  });
-});
--- test/unit/outages.spec.ts
+++ test/unit/outages.spec.ts
@@ -11,18 +11,6 @@ describe('Outage Service', () => {
     mockedAxios.get.mockResolvedValue({ status: 200, data: mockedOutages });
   });
 
-  it('should return a list of outages', async () => {
-    const result = await getOutages();
-    expect(result.length).toBeGreaterThan(0);
-  });
-
-  it('get id, begin, end of outages', async () => {
-    const result = await getOutages();
-    expect(result[0]).toHaveProperty('id');
-    expect(result[0]).toHaveProperty('begin');
-    expect(result[0]).toHaveProperty('end');
-  });
-
   it('should get expected outages from Kraken Api/ outages endpoint', async () => {
     const krakenApi = 'https://api.krakenflex.systems/interview-tests-mock-api/v1/outages';
     const result = await getOutages();
