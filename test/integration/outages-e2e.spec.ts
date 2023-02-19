668c360562ae8b5954b090b3b25c5a9c3f6df44d
Jon Earl <jon.r.earl@gmail.com>
2023-02-19 20:20:05 +0000

    integration test for siteid

--- test/integration/outages-e2e.spec.ts
+++ test/integration/outages-e2e.spec.ts
@@ -1,19 +1,29 @@
 import OutageService from '../../src/outage-service';
+import expectedSiteInfoNorwich from './expected-site-info-norwich.json';
 
 const outageService = new OutageService();
 
 describe('Outage Service', () => {
-  it('should retrieve outages from kraken api /outages endpoint', async () => {
-    const result = await outageService.getOutages();
-    expect(result).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(String),
-          begin: expect.any(String),
-          end: expect.any(String),
-        }),
-      ])
-    );
-    expect(result.length).toEqual(108);
+  describe('Get Outages', () => {
+    it('should retrieve outages from kraken api /outages endpoint', async () => {
+      const result = await outageService.getOutages();
+      expect(result).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+            begin: expect.any(String),
+            end: expect.any(String),
+          }),
+        ])
+      );
+      expect(result.length).toEqual(108);
+    });
+  });
+
+  describe('Get Site Info', () => {
+    it('should get info from site norwich-pear-tree ', async () => {
+      const result = await outageService.getSiteInfo('norwich-pear-tree');
+      expect(result).toEqual(expectedSiteInfoNorwich);
+    });
   });
 });
